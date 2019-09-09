e10b8e55f44a3841205528577b106d3f2d11b1b4
lucasPDY <dpha0008@student.monash.edu>
2019-09-09 17:58:52 +1000

    Finished Week 7 Lab

--- Week 7 Lab/app.js
+++ Week 7 Lab/app.js
@@ -18,10 +18,6 @@ app.use(express.static('css'));
 app.use(bodyparser.urlencoded({ extended: false }));
 app.listen(8080);
 
-
-//reference to the database (i.e. collection)
-let db;
-
 mongoose.connect('mongodb://localhost:27017/week7lab', function (err) {
     if (err) {
         console.log('Error in Mongoose connection');
@@ -29,6 +25,51 @@ mongoose.connect('mongodb://localhost:27017/week7lab', function (err) {
     }
     console.log('Successfully connected');
 });
+
+// let developer1 = new Developer({
+//     _id: new mongoose.Types.ObjectId(),
+//     name: {
+//         firstName: 'Tim',
+//         lastName: 'John'
+//     },
+//     level: "BEGINNER",
+//     address: {
+//         state: "Selangor",
+//         suburb: "Klang",
+//         street: "Lan Street",
+//         unit: "1"
+//     }
+// })
+
+// developer1.save(function(err) {
+//     if (err) {
+//         console.log('Error lmao')
+//     }
+//     console.log("Developer Added");
+// });
+
+// let task1 = new Task({
+//     _id: new mongoose.Types.ObjectId(),
+
+//     name: "Clean",
+//     assignTo: developer1._id, 
+//     dueDate: Date.now(), 
+//     status: "InProgress",
+//     description: "Clean"
+    
+    
+// })
+
+// task1.save(function(err) {
+//     if (err) {
+//         console.log(err);
+//     }else{
+//         console.log("Tasks Added");
+//     }
+// })
+
+
+
 
 //Routes Handlers
 //Insert new Task
@@ -41,20 +82,68 @@ app.get('/insertTask', function (req, res) {
     res.render("insertTask.html");
 })
 
+app.get('/insertDeveloper', function (req, res) {
+    res.render("insertDeveloper.html");
+})
+
 //POST request: receive the details from the client and insert new document (i.e. object) to the collection (i.e. table)
 app.post('/addnewTask', function (req, res) {
     let TaskDetails = req.body;
-    db.collection('Tasks').insertOne({ name: TaskDetails.taskName, person: TaskDetails.taskPerson, date: TaskDetails.taskDate, 
-        status: TaskDetails.taskStatus, description: TaskDetails.taskDescription });
-        res.redirect("/getTasks");
+    let task1 = new Task({
+        _id: new mongoose.Types.ObjectId(),
+        name: TaskDetails.taskName,
+        assignTo: TaskDetails.taskPerson, 
+        dueDate: TaskDetails.taskDate, 
+        status: TaskDetails.taskStatus,
+        description: TaskDetails.taskDescription
+    })
+    task1.save(function (err) {
+        if (err) {
+            console.log(err);
+            console.log("CB cannot add");
+        }
+    })
+    res.redirect("/getTasks");
 });
+
+app.post("/addnewDev", function (req, res) {
+    let devDetails = req.body;
+    let newDev = Developer({
+        _id: new mongoose.Types.ObjectId(),
+        name: {
+            firstName: devDetails.devFirstName, 
+            lastName: devDetails.devLastName
+        },
+        level: devDetails.devLevel,
+        address: {
+            state: devDetails.devState,
+            suburb: devDetails.devSuburb,
+            street: devDetails.devStreet,
+            unit: devDetails.devUnit
+        }
+
+    })
+    newDev.save(function (err) {
+        if (err) {
+            console.log(err);
+        }
+    });
+    res.redirect("/listDeveloper");
+})
 //List all Tasks
 //GET request: send the page to the client. Get the list of documents form the collections and send it to the rendering engine
 app.get('/getTasks', function (req, res) {
-    db.collection('Tasks').find({}).toArray(function (err, data) {
-        res.render('listTasks', { TasksDb: data });
+    Task.find({}, function (err, docs){
+        res.render('listTasks', { TasksDb: docs });
     });
+
 });
+
+app.get('/listDeveloper', function (req, res) {
+    Developer.find({}, function(err, docs){
+        res.render('listDeveloper', { DeveloperDb: docs });
+    })
+})
 //Update Task: 
 //GET request: send the page to the client 
 app.get('/updateTask', function (req, res) {
@@ -65,7 +154,9 @@ app.post('/updateTaskdata', function (req, res) {
     let TaskDetails = req.body;
     let filter = { _id: ObjectID(TaskDetails.taskID)};
     let theUpdate = { $set: { status: TaskDetails.taskNewStatus} };
-    db.collection('Tasks').updateOne(filter, theUpdate);
+    Task.updateOne(filter,theUpdate, function(err, doc) {
+        console.log(doc);
+    })
     res.redirect('/getTasks');// redirect the client to list Tasks page
 })
 //Update Task: 
@@ -76,14 +167,14 @@ app.get('/deleteTask', function (req, res) {
 //POST request: receive the Task's name and do the delete operation 
 app.post('/deleteTaskdata', function (req, res) {
     let TaskDetails = req.body;
-    let filter = { _id: ObjectID(TaskDetails.taskID)};
-    db.collection('Tasks').deleteOne(filter);
+    let filter = { _id: TaskDetails.taskID};
+    Task.deleteOne(filter, function(err, doc) {console.log(doc);});
     res.redirect('/getTasks');// redirect the client to list Tasks page
 });
 
 app.get("/deleteAll", function (req, res) {
-    db.collection("Tasks").deleteMany({status: "Completed"}, function (err, obj) {
-        console.log(obj.result);
+    Task.deleteMany({status: "Completed"}, function (err, doc) {
+        console.log(doc);
       });
     res.redirect('/getTasks');// redirect the client to list Tasks page
 })
