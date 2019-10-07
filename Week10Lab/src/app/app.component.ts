69dd3bd2ae57593e7d2280a2e9886dc3ff2292f9
lucasPDY <dpha0008@student.monash.edu>
2019-10-08 02:31:59 +1100

    Added better navigability

--- Week10Lab/src/app/app.component.ts
+++ Week10Lab/src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Week10Lab';
+  section = 1;
+  changeSection(sectionId) {
+    this.section = sectionId;
+  }
 }
